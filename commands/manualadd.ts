56be1c748a642b2475b890a1c8b80995c0431d9b
Jason Liu <a.flaming.snowman@gmail.com>
2022-08-12 11:21:20 -0700

    Add support for manually multiple users at once (mostly for dev purposes)

--- commands/manualadd.ts
+++ commands/manualadd.ts
@@ -15,14 +15,37 @@ module.exports = {
 			option.setName('lobby')
 				.setDescription('lobby id')
 				.setRequired(true))
+		.addUserOption(option => 
+			option.setName('user2')
+				.setDescription('User to add')
+				.setRequired(false))
+		.addUserOption(option => 
+			option.setName('user3')
+				.setDescription('User to add')
+				.setRequired(false))
+		.addUserOption(option => 
+			option.setName('user4')
+				.setDescription('User to add')
+				.setRequired(false))
+		.addUserOption(option => 
+			option.setName('user5')
+				.setDescription('User to add')
+				.setRequired(false))
 		.setDMPermission(false)
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction: ChatInputCommandInteraction) {
 		const gid = BigInt(interaction.guildId!);
 		const globby = GLOBBY.get(gid);
 		const lobby = globby.get(interaction.options.getInteger('lobby', true));
-		const resp = lobby!.memberJoin(BigInt(interaction.options.getUser('user', true).id));
-		await interaction.reply({ content: String(resp) });
+		let resp = String(lobby!.memberJoin(BigInt(interaction.options.getUser('user', true).id)));
+		for(let i = 2; i <= 5; i++) {
+			const user = interaction.options.getUser(`user${i}`, false);
+			if(!user) {
+				continue;
+			}
+			resp += ' ' + String(lobby!.memberJoin(BigInt(interaction.options.getUser(`user${i}`)!.id)));
+		}
+		await interaction.reply({ content: resp });
 	},
 };
 
